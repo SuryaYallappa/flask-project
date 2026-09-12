72a843a6ce297805c3406d7efaa8ccf18a6a82b4
Surya Y <suryayallappa@gmail.com>
2026-09-12 11:56:46 +0530

    Added to-do page

---
+++ todo-app/frontend/app.py
@@ -0,0 +1,30 @@
+import requests
+from flask import Flask, render_template, request
+
+BACKEND_URL = "http://127.0.0.1:9000"
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    from_data = dict(request.form)
+
+    requests.post(f"{BACKEND_URL}/submit", json=from_data)
+
+    return "Data submitted successfully!"
+
+
+@app.route("/view")
+def view():
+    response = requests.get(f"{BACKEND_URL}/view")
+
+    return response.json()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
